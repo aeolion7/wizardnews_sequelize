2b4abaf4795cd45d9a8ed1d1c5c8b6693dcdeac2
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-11 13:12:40 -0500

    part 2 complete

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const postBank = require('./postBank');
-const postDetails = require('./postDetails');
-const postList = require('./postList');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
+app.use('/posts', require('./routes/posts'));
+
 app.get('/', (req, res) => {
-  const posts = postBank.list();
-  res.send(postList(posts));
-});
-
-app.get('/posts/:id', (req, res) => {
-  const post = postBank.find(req.params.id);
-
-  res.send(postDetails(post));
+  res.redirect('/posts');
 });
 
 const PORT = 1337;
