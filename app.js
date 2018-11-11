e4f78c699311fba608573a9d7f48f3bc296e0f7c
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-11 12:07:20 -0500

    added first dynamic routing

--- app.js
+++ app.js
@@ -1,7 +1,62 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+const postBank = require('./postBank');
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class="news-item">
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+                <a href="posts/${post.id}">${post.title}</a>
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>`
+
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  res.send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/><a href="/">Wizard News</a></header>
+        <h1>${post.title} <small>(by ${post.name})</small></h1>
+        <p>${post.content}</p>
+      </div>
+    </body>
+  </html>`);
+});
 
 const PORT = 1337;
 
