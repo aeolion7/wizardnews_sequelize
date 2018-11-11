df82265fb81c2ccdc48c39dd56c6ba70d808458c
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-11 13:28:25 -0500

    beginning sequelize

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.use('/posts', require('./routes/posts'));
